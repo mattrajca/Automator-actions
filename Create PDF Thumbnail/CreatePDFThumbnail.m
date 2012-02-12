8871e7d5d7c393394fde32ef0b80bbd008e73740
Matt Rajca <matt.rajca@me.com>
2012-02-12 13:42:27 -0600

    Added some logging to the 'Create PDF Thumbnail' action

--- Create PDF Thumbnail/CreatePDFThumbnail.m
+++ Create PDF Thumbnail/CreatePDFThumbnail.m
@@ -15,9 +15,24 @@
 	NSMutableArray *output = [[NSMutableArray alloc] init];
 	
 	for (NSString *path in input) {
+		if (![[path pathExtension] isEqualToString:@"pdf"]) {
+			[self logMessageWithLevel:AMLogLevelWarn format:@"Skipping '%@' since it is not a PDF", path];
+			continue;
+		}
+		
 		NSURL *url = [NSURL fileURLWithPath:path];
 		
+		if (!url) {
+			[self logMessageWithLevel:AMLogLevelError format:@"The path '%@' is invalid", path];
+			continue;
+		}
+		
 		PDFDocument *doc = [[PDFDocument alloc] initWithURL:url];
+		
+		if (!doc) {
+			[self logMessageWithLevel:AMLogLevelError format:@"Could not load the PDF document: %@", path];
+			continue;
+		}
 		
 		if ([doc pageCount] > 0) {
 			PDFPage *page = [doc pageAtIndex:0];
@@ -27,12 +42,16 @@
 			NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithData:[image TIFFRepresentation]];
 			[image release];
 			
+			NSData *data = [rep representationUsingType:NSJPEGFileType properties:nil];
+			[rep release];
+			
+			NSError *error = nil;
 			NSString *newPath = [[path stringByDeletingPathExtension] stringByAppendingPathExtension:@"jpg"];
 			
-			[[rep representationUsingType:NSJPEGFileType properties:nil] writeToFile:newPath
-																		  atomically:YES];
-			
-			[rep release];
+			if (![data writeToFile:newPath options:NSDataWritingAtomic error:&error]) {
+				[self logMessageWithLevel:AMLogLevelError format:@"Could not write the thumbnail to '%@'. Error: %@", newPath, error];
+				continue;
+			}
 			
 			[output addObject:newPath];
 		}
